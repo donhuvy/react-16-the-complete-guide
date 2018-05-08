0e0de1062d91c3a6b67f887721b1a80c81a056eb
Thiago Barreto <tbarretoc@gmail.com>
2018-05-08 11:54:59 +0200

    Up to section 14: Redux

---
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+import * as actionTypes from '../store/actions'
+
+class Persons extends Component {
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.addPerson} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.deletePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        persons: state.persons
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, payload: {name: name, age: age}}),
+        deletePerson: (id) => dispatch({type: actionTypes.DELETE_PERSON, personId: id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
